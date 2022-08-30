3b173461fbd773a5d62ec671e1325b7bdc01374a
Brenda_Kiage <92789637+Kiage24@users.noreply.github.com>
2022-08-30 13:32:22 +0300

    Update 03hisat2.sh

--- scripts/03hisat2.sh
+++ scripts/03hisat2.sh
@@ -2,13 +2,13 @@
 
 # Download the reference genome
 
-#wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/499/845/GCF_000499845.1_PhaVulg1_0/GCF_000499845.1_PhaVulg1_0_genomic.fna.gz
-#gunzip GCF_000499845.1_PhaVulg1_0_genomic.fna.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/499/845/GCF_000499845.1_PhaVulg1_0/GCF_000499845.1_PhaVulg1_0_genomic.fna.gz
+gunzip GCF_000499845.1_PhaVulg1_0_genomic.fna.gz
 
 #Build the index file
 
-#mv GCF_000499845.1_PhaVulg1_0_genomic.fna ../alignment/
-#hisat2-build -f ../alignment/GCF_000499845.1_PhaVulg1_0_genomic.fna ../alignment/GCF_000499845.1_PhaVulg1_0_genomic.fna.idx
+mv GCF_000499845.1_PhaVulg1_0_genomic.fna ../alignment/
+hisat2-build -f ../alignment/GCF_000499845.1_PhaVulg1_0_genomic.fna ../alignment/GCF_000499845.1_PhaVulg1_0_genomic.fna.idx
 
 #Run the alignment
 
