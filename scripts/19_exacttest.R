625cdaecd31607eb2eb50638297a48275138adbd
Brenda_Kiage <92789637+Kiage24@users.noreply.github.com>
2022-09-07 11:18:51 +0300

    Update 19_exacttest.R

--- scripts/19_exacttest.R
+++ scripts/19_exacttest.R
@@ -17,21 +17,3 @@ pinto_et30_15 <- exactTest(pinto_d, pair = c('30','15'))
 pinto_et30_20<- exactTest(pinto_d, pair = c('30','20'))
 pinto_et30_35 <- exactTest(pinto_d, pair = c('30','35'))
 
-
-#Extract the most differentially expressed genes for each variety conditions
-canadian_topTags30_15 <- topTags(canadian_et30_15)
-canadian_topTags30_20 <- topTags(canadian_et30_20)
-canadian_topTags30_35 <- topTags(canadian_et30_35)
-
-rosecoco_topTags30_15 <- topTags(rosecoco_et30_15)
-rosecoco_topTags30_20 <- topTags(rosecoco_et30_20)
-rosecoco_topTags30_35 <- topTags(rosecoco_et30_35)
-rosecoco_topTags30_30df <- topTags(rosecoco_et30_30df)
-
-pinto_topTags30_15 <- topTags(pinto_et30_15)
-pinto_topTags30_20 <- topTags(pinto_et30_20)
-pinto_topTags30_35 <- topTags(pinto_et30_35)
-
-canadian_topTags30_15
-pinto_topTags30_20
-rosecoco_topTags30_20
